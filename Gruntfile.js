f3cb4ee520c035b289ae39d732a992ae60eb5e87
jwicksnin <jwicksnin@gmail.com>
2014-02-12 21:46:29 -0800

    Tidying up code

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,9 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-
-      //globals: //to be ok, etc
       allFiles: [
         'Gruntfile.js',
         'test/*.js',
-        // '<%= nodeunit.tests %>',
       ]
     },
     simplemocha: {
